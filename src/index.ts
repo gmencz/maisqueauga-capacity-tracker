45dec01512ecc33d2f8c9dbb61880ffad9dcb126
Gabriel Méndez <yo@gabrielmendezc.com>
2021-08-30 11:34:58 +0200

    Add logging

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,20 @@ import { promises as fs, constants as fsConstants } from "fs";
 import { CronJob } from "cron";
 import puppeteer from "puppeteer";
 import { format } from "date-fns";
+import pino from "pino";
 
 const EVERY_30_MINUTES = "*/30 * * * *";
 const CAPACITY_URL = "https://maisqueauga.com/es/aforo-2/";
 
-const job = new CronJob(EVERY_30_MINUTES, async () => {
+const logger = pino({
+  prettyPrint: {
+    translateTime: true,
+  },
+});
+
+const takeCapacityScreenshot = async () => {
+  logger.info("Taking capacity screenshot...");
+
   const screenshotsDir = `screenshots/${format(new Date(), "d-M-y")}`;
 
   try {
@@ -24,6 +33,10 @@ const job = new CronJob(EVERY_30_MINUTES, async () => {
   });
 
   await browser.close();
-});
+};
 
-job.start();
+// Take a initial screenshot.
+takeCapacityScreenshot();
+
+// Take a screenshot every 30th minute.
+const job = new CronJob(EVERY_30_MINUTES, takeCapacityScreenshot, null, true);
